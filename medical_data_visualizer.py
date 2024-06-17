04c5e99eff57fe3122c4b7b0c829af33cbd764e8
Louise Wong <hauying101@gmail.com>
2024-06-17 16:26:08 +0000

    added catplot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,36 +4,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
 df['overweight'] = None
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].map({1: 0, 2: 1, 3:1})
+df['gluc'] = df['gluc'].map({1: 0, 2: 1, 3:1})
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
+    
 
     # 6
-    df_cat = None
-    
+    df_cat['count']=1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index=False).count()
 
     # 7
 
-
+    catplot = sns.catplot(data=df_cat, x='variable', y='count', hue='value', col="cardio", kind='bar')
 
     # 8
-    fig = None
+    fig = catplot
 
 
     # 9
     fig.savefig('catplot.png')
     return fig
-
+draw_cat_plot()
 
 # 10
 def draw_heat_map():
