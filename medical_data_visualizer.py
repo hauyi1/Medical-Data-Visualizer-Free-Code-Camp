0e069e2eb9f63bee38566ede09c39e18b9192dc0
Louise Wong <hauying101@gmail.com>
2024-06-24 15:27:01 +0000

    Added heatmap

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -39,23 +39,32 @@ draw_cat_plot()
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[((df['ap_lo'] <= df['ap_hi'])) & 
+                (df['height'] >= df['height'].quantile(0.025)) &
+                (df['height'] <= df['height'].quantile(0.975)) & 
+                (df['weight'] >= df['weight'].quantile(0.025)) & 
+                (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones(corr.shape)).astype(bool)
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(10, 8))
 
-    # 15
+
+    # 15 
+    sns.heatmap(corr, mask=mask, annot=True, ax=ax, fmt="0.1f")
+ 
 
 
 
     # 16
     fig.savefig('heatmap.png')
     return fig
+
+draw_heat_map()
