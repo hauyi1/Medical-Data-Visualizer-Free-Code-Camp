344716f7d5a016b71cf9a06d552035efd925c920
Louise Wong <hauying101@gmail.com>
2024-07-10 11:17:07 +0000

    Fixed errors

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,7 +7,8 @@ import numpy as np
 df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25)
+df['overweight'] = df['overweight'].map({True: 1, False: 0})
 
 # 3
 df['cholesterol'] = df['cholesterol'].map({1: 0, 2: 1, 3:1})
@@ -26,27 +27,33 @@ def draw_cat_plot():
     # 7
 
     catplot = sns.catplot(data=df_cat, x='variable', y='count', hue='value', col="cardio", kind='bar')
+    catplot.set_axis_labels('variable', 'total')
+
 
     # 8
-    fig = catplot
-
+    fig = catplot.fig
+    
 
     # 9
     fig.savefig('catplot.png')
     return fig
-draw_cat_plot()
 
 # 10
 def draw_heat_map():
+    lower_quantile_height = df['height'].quantile(0.025)
+    upper_quantile_height = df['height'].quantile(0.975)
+    lower_quantile_weight = df['weight'].quantile(0.025)
+    upper_quantile_weight = df['weight'].quantile(0.975)
+
     # 11
-    df_heat = df[((df['ap_lo'] <= df['ap_hi'])) & 
-                (df['height'] >= df['height'].quantile(0.025)) &
-                (df['height'] <= df['height'].quantile(0.975)) & 
-                (df['weight'] >= df['weight'].quantile(0.025)) & 
-                (df['weight'] <= df['weight'].quantile(0.975))]
+    df_heat = df[(df['height'] >= lower_quantile_height) &
+                (df['height'] <= upper_quantile_height) & 
+                (df['weight'] >= lower_quantile_weight) & 
+                (df['weight'] <= upper_quantile_weight)]
 
     # 12
-    corr = df_heat.corr()
+    corr = df_heat.corr(numeric_only=False)
+
 
     # 13
     mask = np.triu(np.ones(corr.shape)).astype(bool)
@@ -59,12 +66,10 @@ def draw_heat_map():
 
     # 15 
     sns.heatmap(corr, mask=mask, annot=True, ax=ax, fmt="0.1f")
- 
+    plt.show()
 
 
 
     # 16
     fig.savefig('heatmap.png')
     return fig
-
-draw_heat_map()
